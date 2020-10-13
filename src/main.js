37262a01b9edc6d23c5ae1ca4b760b7782937a37
TakeshiHora <a1936th@aiit.ac.jp>
2020-10-14 00:37:50 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue/dist/vue.esm.js";
+import axios from 'axios'
+//import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+/*new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
+
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+    //.get("/disaster?qzqsmdata=9AAFAB9ED64007D1F465F841F192000802DFB300F7ED004FFFFFA0100000004")
+    .get("/list")
+    .then(response => (this.info = response))
+  }
+})
